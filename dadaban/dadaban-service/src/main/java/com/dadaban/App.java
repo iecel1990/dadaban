09793f1fc19b623a7ac2e2143f190a4cfb0af09b
jrose <iecel1990@gmail.com>
2014-07-22 14:01:58 +0800

    init project

---
+++ dadaban/dadaban-service/src/main/java/com/dadaban/App.java
@@ -0,0 +1,13 @@
+package com.dadaban;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
